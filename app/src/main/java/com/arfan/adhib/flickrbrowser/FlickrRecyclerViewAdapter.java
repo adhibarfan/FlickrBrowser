4832486d4e11937f71d909ac2a02dccec2cffef8
adhibarfan <adhib.arfan@gmail.com>
2017-03-23 18:05:47 +0700

    add flickr recycler view adapter

--- app/src/main/java/com/arfan/adhib/flickrbrowser/FlickrRecyclerViewAdapter.java
+++ app/src/main/java/com/arfan/adhib/flickrbrowser/FlickrRecyclerViewAdapter.java
@@ -1,8 +1,75 @@
 package com.arfan.adhib.flickrbrowser;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
 /**
  * Created by emerio on 3/23/2017.
  */
 
-class FlickrRecyclerViewAdapter {
+class FlickrRecyclerViewAdapter extends RecyclerView.Adapter<FlickrRecyclerViewAdapter.FlickrImageViewHolder> {
+    private static final String TAG = "FlickrRecyclerViewAdapt";
+    private List<Photo> mPhotosList;
+    private Context mContext;
+
+    public FlickrRecyclerViewAdapter(Context context, List<Photo> photoList) {
+        this.mContext = context;
+        this.mPhotosList = photoList;
+    }
+
+    @Override
+    public FlickrImageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // Called by the layout manager when it needs a new layout
+        Log.d(TAG, "onCreateViewHolder: new view requested");
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.browse, parent, false);
+        return new FlickrImageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(FlickrImageViewHolder holder, int position) {
+
+        
+
+    }
+
+    @Override
+    public int getItemCount() {
+        Log.d(TAG, "getItemCount: called");
+//        if ((mPhotosList != null) && (mPhotosList.size() != 0)){
+//            return mPhotosList.size();
+//        }else {
+//            return 0;
+//        }
+        return ((mPhotosList != null) && (mPhotosList.size() != 0) ? mPhotosList.size() : 0);
+    }
+
+    void loadNewData(List<Photo> newPhotos) {
+        mPhotosList = newPhotos;
+        notifyDataSetChanged();
+    }
+
+    public Photo getPhoto(int position) {
+        return ((mPhotosList != null) && (mPhotosList.size() != 0) ? mPhotosList.get(position) : null);
+    }
+
+    static class FlickrImageViewHolder extends RecyclerView.ViewHolder {
+        private static final String TAG = "FlickrImageViewHolder";
+        ImageView thumbnail = null;
+        TextView title = null;
+
+        public FlickrImageViewHolder(View itemView) {
+            super(itemView);
+            Log.d(TAG, "FlickrImageViewHolder: starts");
+            this.thumbnail = (ImageView) itemView.findViewById(R.id.thumbnail);
+            this.title = (TextView) itemView.findViewById(R.id.title);
+        }
+    }
 }
