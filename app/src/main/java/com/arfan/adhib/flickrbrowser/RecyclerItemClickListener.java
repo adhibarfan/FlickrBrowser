8760ee0fd1eee08a2ee18927128f7e588e75b91c
adhibarfan <adhib.arfan@gmail.com>
2017-03-29 15:50:13 +0700

    add recycler item click listener

--- app/src/main/java/com/arfan/adhib/flickrbrowser/RecyclerItemClickListener.java
+++ app/src/main/java/com/arfan/adhib/flickrbrowser/RecyclerItemClickListener.java
@@ -1,8 +1,35 @@
 package com.arfan.adhib.flickrbrowser;
 
+import android.content.Context;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
 /**
  * Created by emerio on 3/29/2017.
  */
 
-class RecyclerItemClickListener {
+class RecyclerItemClickListener extends RecyclerView.SimpleOnItemTouchListener{
+    private static final String TAG = "RecyclerItemClickListen";
+
+    interface OnRecyclerClickListener{
+        void onItemClick(View view, int position);
+        void onItemLongClick(View view, int position);
+    }
+
+    private final OnRecyclerClickListener mListener;
+    private final GestureDetectorCompat mGestureDetector;
+
+    public RecyclerItemClickListener(Context context, final RecyclerView recyclerView, OnRecyclerClickListener listener) {
+        mListener = listener;
+        mGestureDetector = null;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+        Log.d(TAG, "onInterceptTouchEvent: starts");
+        return super.onInterceptTouchEvent(rv, e);
+    }
 }
